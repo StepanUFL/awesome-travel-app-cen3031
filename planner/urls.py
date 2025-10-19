f7f80a0518d766cb0efbff84316cd115bf544afd
westenral <137741690+westenral@users.noreply.github.com>
2025-10-18 21:08:20 -0400

    Add basic views for some GET/POST requests

--- planner/urls.py
+++ planner/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+# app_name = "planner"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("location/", views.location_info, name="location-info"),
+    path("route/", views.route_info, name="route-info"),
+    path("new-route/", views.new_route, name="new-route"),
 ]
--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,57 @@
-from django.http import HttpRequest, HttpResponse, FileResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
+
+users = {}
+
+locations = {
+    "america": {
+        "display_name": "America",
+    },
+    "brazil": {
+        "display_name": "Brazil",
+    },
+}
+
+routes = {}
 
 
-def index(request):
+# View for the index page
+def index(request: HttpRequest):
     return render(request, "index.html", {})
+
+
+# View that responds to GET requests for information about a location
+# URL: /location/
+# URL Parameters:
+#   id: The ID of the location
+def location_info(request: HttpRequest):
+    location_id = request.GET["id"]
+    return JsonResponse(locations[location_id])
+
+
+# View that responds to POST requests to create a new route
+# URL: /new-route/
+# Payload Parameters:
+#   route_id: The ID that the route should be created under
+#   locations_ids: A comma-separated list of location IDs
+def new_route(request: HttpRequest):
+    route_id = request.POST["route_id"]
+
+    location_ids = request.POST["location_ids"].split(sep=",")
+    route = []
+    for id in location_ids:
+        route.append(id)
+
+    routes[route_id] = route
+
+    return HttpResponseRedirect(reverse("index"))
+
+
+# View that responds to GET requests for information about a route
+# URL: /route/
+# URL Parameters:
+#   id: The ID of the route
+def route_info(request: HttpRequest):
+    route_id = request.GET["id"]
+    return JsonResponse(routes[route_id])
