461ae1a7ecb95250f69d9475afba965ebe2d632b
Noah Swor <noahpswor@gmail.com>
2025-10-28 15:04:42 -0400

    database setup

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# creating a user with passwrod for database
+from django.contrib.auth.hashers import make_password
+
+class SimpleUser(models.Model):
+    userID   = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    
+
+    def set_password(self, raw):
+        self.password = make_password(raw)
+
+
+
 
 # Create your models here.
