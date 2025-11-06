42ab29433e14b2dd14ea3d14cfc69c6950606349
westenral <137741690+westenral@users.noreply.github.com>
2025-11-06 16:42:27 -0500

    Remove SimpleUser model

--- planner/models.py
+++ planner/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.contrib.auth.hashers import make_password
 
 
-# creating a user with password for database
-class SimpleUser(models.Model):
-    userID = models.AutoField(primary_key=True)
-    userName = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=128)
-
-    def set_password(self, raw):
-        self.password = make_password(raw)
-
-
 class Route(models.Model):
     location_ids = models.JSONField(null=True)
 
