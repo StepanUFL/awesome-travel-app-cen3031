81e31ab1a72ffe6a9b3b21f57ec251a2bd20f84b
Noah Swor <noahpswor@gmail.com>
2025-11-30 17:07:16 -0500

    login features additions and fixes

--- planner/views.py
+++ planner/views.py
@@ -2,41 +2,59 @@ import json
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
-from .models import Route
-
+from django.db import IntegrityError
+from django.contrib.auth.hashers import check_password
+from .models import Route, SimpleUser
 
 # View for the index page
 def index(request: HttpRequest):
     return render(request, "index.html", {})
 
-
 def login(request: HttpRequest):
+    if request.method == "POST":
+        username = (request.POST.get("userName") or request.POST.get("username") or "").strip()
+        password = request.POST.get("password") or ""
+        if not username or not password:
+            return render(request, "login.html", {"error": "Username and password are required.", "userName": username})
+
+        try:
+            user = SimpleUser.objects.get(userName=username)
+        except SimpleUser.DoesNotExist:
+            return render(request, "login.html", {"error": "Invalid username or password.", "userName": username})
+
+        if check_password(password, user.password):
+            # this is from CHATGPT, I did not know how to create "sessions" to remember the created username to display it
+            request.session["simple_user_id"] = user.pk
+            request.session["simple_user_name"] = user.userName
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html", {"error": "Invalid username or password.", "userName": username})
+
     return render(request, "login.html")
 
-
 def signin(request: HttpRequest):
+    if request.method == "POST":
+        username = (request.POST.get("userName") or "").strip()
+        password = request.POST.get("password") or ""
+        if not username or not password:
+            return render(request, "signin.html", {"error": "Username and password are required.", "userName": username})
+        user = SimpleUser(userName=username)
+        user.set_password(password)
+        try:
+            user.save()
+        except IntegrityError:
+            return render(request, "signin.html", {"error": "Username already taken.", "userName": username})
+        return HttpResponseRedirect(reverse("login"))
     return render(request, "signin.html")
 
-
 def main(request: HttpRequest):
-    return render(request, "main.html")
+    # as described above, session stuff is from CHATGPT
+    current_username = request.session.get("simple_user_name")
+    return render(request, "main.html", {"current_username": current_username})
 
-
-# URL: /location/{id}
-# View that responds to GET requests for information about a location
-#
-# GET: Returns a JSON object containing information about the location
 def location(request: HttpRequest, id):
     return JsonResponse({"nope": "nope"})
 
-
-# URL: /route/{id}
-# View that updates routes or returns route info
-#
-# GET: Returns a JSON object with the route with the given ID
-#
-# POST: Updates the route with the given ID, or creates a new one
-# Body should be JSON data with a list named "location_ids"
 def route(request: HttpRequest, id: int):
     if request.method == "POST":
         location_ids_json = json.loads(request.body)
@@ -44,7 +62,5 @@ def route(request: HttpRequest, id: int):
         new_route.pk = id
         new_route.save()
         return HttpResponseRedirect(reverse("index"))
-
     elif request.method == "GET":
         return JsonResponse(Route.objects.get(pk=id).location_ids)
-        # return JsonResponse(routes[id], safe=False)
