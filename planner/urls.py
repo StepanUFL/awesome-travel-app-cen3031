759e784f8a039c629a5056fe982b900766afe14b
Noah Swor <noahpswor@gmail.com>
2025-12-01 12:04:17 -0500

    resolve merge conflicts and fix stupid stuff

--- planner/urls.py
+++ planner/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-
 from . import views
 
-# app_name = "planner"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path('login', views.login, name="login"),
-    path('signin', views.signin, name="signin"),
-    path('main', views.main, name="main"),
+    path("login", views.login, name="login"),
+    path("signin", views.signin, name="signin"),
+    path("main", views.main, name="main"),
     path("location/<id>/", views.location, name="location"),
     path("route/<int:id>/", views.route, name="route"),
+    path("admin-users", views.admin_users, name="admin_users"),
 ]
